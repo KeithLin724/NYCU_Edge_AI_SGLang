9dec3bb359e40e31a1f17640bdc851caf1bd89ba
KeithLin724 <keithlin724@gmail.com>
2025-05-31 22:14:41 +0800

    speed up

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,6 @@
 # reference : https://docs.sglang.ai/backend/quantization.html
+# reference : https://docs.sglang.ai/backend/server_arguments.html
+# reference : https://docs.sglang.ai/backend/hyperparameter_tuning.html
 
 python3 -m sglang.launch_server --model-path Llama-3.2-3B-Instruct-W8A8-Dynamic-Per-Token-V2 \
  --tp 1 \
@@ -7,4 +9,9 @@ python3 -m sglang.launch_server --model-path Llama-3.2-3B-Instruct-W8A8-Dynamic-
  --context-length 4096 \
  --enable-torch-compile \
  --attention-backend flashinfer \
- --enable-mixed-chunk 
+ --enable-mixed-chunk \
+ --schedule-policy fcfs --schedule-conservativeness 0.8
+
+
+
+
