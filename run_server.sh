0d5c01960b902501daf475ebffd3fb17de979b18
KeithLin724 <keithlin724@gmail.com>
2025-05-31 18:24:37 +0800

    update load quant model

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
-python3 -m sglang.launch_server --model-path meta-llama/Llama-3.2-3B-Instruct \
+python3 -m sglang.launch_server --model-path Llama-3.2-3B-Instruct-W8A8-Dynamic-Per-Token-One \
  --tp 2 \
  --host 0.0.0.0 \
  --context-length 4096 \
