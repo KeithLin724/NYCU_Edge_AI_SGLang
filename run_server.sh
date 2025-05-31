09736108c87780b34fb205bd92df620df172f49d
KeithLin724 <keithlin724@gmail.com>
2025-05-31 21:42:26 +0800

    update only use one card

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 # reference : https://docs.sglang.ai/backend/quantization.html
 
 python3 -m sglang.launch_server --model-path Llama-3.2-3B-Instruct-W8A8-Dynamic-Per-Token-V2 \
- --tp 2 \
+ --tp 1 \
  --quantization w8a8_int8 \
  --host 0.0.0.0 \
  --context-length 4096 \
