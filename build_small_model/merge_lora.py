cc2d7a7296faa42571b86c4fa40511fc7f2a92e2
KeithLin724 <keithlin724@gmail.com>
2025-06-01 14:43:27 +0800

    add build small model doc

---
+++ build_small_model/merge_lora.py
@@ -0,0 +1,16 @@
+from peft import PeftModel
+from transformers import AutoModelForCausalLM
+import torch
+
+# Load base quantized model
+base_model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Llama-3.2-1B-Instruct",
+    device_map="auto",
+    torch_dtype=torch.float16,
+)
+
+# Load PEFT adapters
+model = PeftModel.from_pretrained(base_model, "<YOUR_OUTPUT_DIR>/epoch_0")
+
+model = model.merge_and_unload()
+model.save_pretrained("<YOUR_MERGED_MODEL_PATH>")
